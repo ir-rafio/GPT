ea1811fbf76e45be61d60c76074ab16ae8066a3d
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-04-28 23:29:39 +0600

    more styling stuff

--- packages/client/src/components/ui/nicknames.tsx
+++ packages/client/src/components/ui/nicknames.tsx
@@ -24,15 +24,20 @@ function Nicknames({ id }: { id: string | undefined }) {
     "Polo",
     "Lili",
     "Mimi",
-    "Kiki"
+    "Kiki",
+    "বাংলাদেশ"
   ];
   const isMe = window.location.pathname.split("/").at(-1) === "me";
 
   console.log("Fetching nicknames for user", id);
   return (
     <>
-      <div className="m-2">
-        <div className="ml-4">Nicknames</div>
+      <div className="m-2 mr-24 mt-8">
+        <div className="ml-4">
+          <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            Nicknames
+          </h2>
+        </div>
         {!isMe && (
           <div className="m-2 ml-4 flex items-center justify-start">
             <input
@@ -50,8 +55,12 @@ function Nicknames({ id }: { id: string | undefined }) {
             <Card key={idx} className="m-2 w-96 p-2">
               <div className="flex items-center justify-between space-x-2">
                 <div>
-                  <div className="justify-start">{nickname}</div>
-                  <div className="text-xs text-gray-500">Votes: {0}</div>
+                  <div className="justify-start">
+                    <code>{nickname}</code>
+                  </div>
+                  <div className="text-xs text-gray-500">
+                    Votes: {0} // TODO: fetch votes
+                  </div>
                 </div>
                 <div>
                   <Button variant="destructive" size="sm" className="mr-2">
