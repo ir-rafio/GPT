91eee25078a53ba4cff36110084a34545f0be338
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-04-30 22:30:52 +0600

    added carousel to /

--- packages/client/src/pages/home.tsx
+++ packages/client/src/pages/home.tsx
@@ -1,6 +1,13 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from "@/components/ui/carousel";
 import { axiosClient } from "@/config/axios";
 import { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
 
 interface UserData {
   metadata: {
@@ -17,21 +24,56 @@ export default function Home() {
     });
   }, []);
 
-  if (userData) {
-    window.localStorage.setItem("id", userData.metadata.studentId);
-    window.localStorage.setItem("name", userData.metadata.name);
-    return <Navigate to="/users/me" replace />;
-  }
   return (
-    <div className="m-6 mt-12 flex justify-center">
+    <div className="m-6 flex justify-center">
       {userData ? (
-        <div className="text-center">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            <code>Hello, {userData.metadata.name}</code>
-          </h1>
+        <div>
+          <div className="m-12 mx-auto w-full text-center">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              <code>Hello, {userData.metadata.name}</code>
+            </h1>
+          </div>
+
+          <div className="flex h-full w-full items-center justify-center">
+            <Carousel className="h-full w-full">
+              <CarouselContent>
+                <CarouselItem key={1}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-4xl font-semibold">{100}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+
+                <CarouselItem key={2}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-4xl font-semibold">{100}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+
+                <CarouselItem key={3}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-4xl font-semibold">{100}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
         </div>
       ) : (
-        <div className="animate-pulse">Loading....</div>
+        <div className="animate-pulse text-xl">Loading....</div>
       )}
     </div>
   );
