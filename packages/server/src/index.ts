387134ed24742278261cc9577e4e7ca2044f72e3
renzhamin <renzhamin@gmail.com>
2024-05-01 23:38:16 +0600

    removed seeding from index

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,14 +1,11 @@
 import app from "./app";
 import { databaseClient } from "./database";
-import { seedNicknames, seedStudents } from "./utils/student";
 
 app.listen(app.get("port"), async () => {
   console.log("Server running on http://localhost:" + app.get("port") + "/");
 
   try {
     await databaseClient.$connect();
-    seedStudents();
-    seedNicknames();
     console.log("Database connection has been established.");
   } catch (error) {
     console.error("Database connection could not be established.");
