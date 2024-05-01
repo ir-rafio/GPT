0199dacd84f702d6c8a462fcf9d359f0ee2f7f8e
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-05-01 21:46:44 +0600

    refactored homepage

--- packages/client/src/pages/home.tsx
+++ packages/client/src/pages/home.tsx
@@ -7,7 +7,6 @@ import {
   CarouselPrevious
 } from "@/components/ui/carousel";
 import { axiosClient } from "@/config/axios";
-import { cse1, cse2, swe } from "@/data/students";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -18,14 +17,38 @@ interface UserData {
   };
 }
 
+interface ClassmatesMap {
+  [group: string]: UserData[];
+}
+
+function createClassmatesMap(classmates: any[]): ClassmatesMap {
+  const classmatesMap: ClassmatesMap = {};
+  for (const classmate of classmates) {
+    const group = Math.floor(classmate.id / 100).toString();
+    if (!classmatesMap[group]) classmatesMap[group] = [];
+
+    const { id, name } = classmate;
+    classmatesMap[group].push({ metadata: { studentId: id, name } });
+  }
+  return classmatesMap;
+}
+
 export default function Home() {
   const [userData, setUserData] = useState<UserData | null>(null);
+  const [allClassmates, setAllClassmates] = useState<any>(null);
+
   useEffect(() => {
     axiosClient.get("/user/data").then((res) => {
       setUserData(res.data);
       window.localStorage.setItem("id", res.data.metadata.studentId);
       window.localStorage.setItem("name", res.data.metadata.name);
     });
+
+    axiosClient
+      .get(`/student/classmate/${userData?.metadata.studentId}/list`)
+      .then((res) => {
+        setAllClassmates(res.data.students);
+      });
   }, []);
 
   const navigate = useNavigate();
@@ -34,7 +57,7 @@ export default function Home() {
 
   return (
     <div className="m-6 flex justify-center">
-      {userData ? (
+      {userData && allClassmates ? (
         <div>
           <div className="m-4 mx-auto w-full text-center">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -49,74 +72,36 @@ export default function Home() {
           <div className="flex items-center justify-center">
             <Carousel className="h-full w-11/12">
               <CarouselContent>
-                <CarouselItem key={1}>
-                  <div className="space-y-4 p-1">
-                    <Card className={bgColor}>
-                      <CardContent className="mt-4 grid grid-cols-5 grid-rows-8 gap-4">
-                        {cse1.map((student, index) => (
-                          <Card
-                            key={index}
-                            className="hover:drop-shadow-lg"
-                            onClick={() => navigate(`/users/${student.id}`)}
-                          >
-                            <CardHeader className="p-4 pb-1 font-semibold">
-                              {student.id}
-                            </CardHeader>
-                            <CardContent className="p-4 pt-1">
-                              <code>{student.name}</code>
-                            </CardContent>
-                          </Card>
-                        ))}
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-
-                <CarouselItem key={2}>
-                  <div className="space-y-4 p-1">
-                    <Card className={bgColor}>
-                      <CardContent className="mt-4 grid grid-cols-5 grid-rows-8 gap-4">
-                        {cse2.map((student, index) => (
-                          <Card
-                            key={index}
-                            className="hover:drop-shadow-lg"
-                            onClick={() => navigate(`/users/${student.id}`)}
-                          >
-                            <CardHeader className="p-4 pb-1 font-semibold">
-                              {student.id}
-                            </CardHeader>
-                            <CardContent className="p-4 pt-1">
-                              <code>{student.name}</code>
-                            </CardContent>
-                          </Card>
-                        ))}
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-
-                <CarouselItem key={3}>
-                  <div className="space-y-4 p-1">
-                    <Card className={bgColor}>
-                      <CardContent className="mt-4 grid grid-cols-5 grid-rows-8 gap-4">
-                        {swe.map((student, index) => (
-                          <Card
-                            key={index}
-                            className="hover:drop-shadow-lg"
-                            onClick={() => navigate(`/users/${student.id}`)}
-                          >
-                            <CardHeader className="p-4 pb-1 font-semibold">
-                              {student.id}
-                            </CardHeader>
-                            <CardContent className="p-4 pt-1">
-                              <code>{student.name}</code>
-                            </CardContent>
-                          </Card>
-                        ))}
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
+                {Object.values(createClassmatesMap(allClassmates)).map(
+                  (cse1, index) => (
+                    <CarouselItem key={index}>
+                      <div className="space-y-4 p-1">
+                        <Card className={bgColor}>
+                          <CardContent className="mt-4 grid grid-cols-5 grid-rows-8 gap-4">
+                            {cse1.map((student, index) => (
+                              <Card
+                                key={index}
+                                className="hover:drop-shadow-lg"
+                                onClick={() =>
+                                  navigate(
+                                    `/users/${student.metadata.studentId}`
+                                  )
+                                }
+                              >
+                                <CardHeader className="p-4 pb-1 font-semibold">
+                                  {student.metadata.studentId}
+                                </CardHeader>
+                                <CardContent className="p-4 pt-1">
+                                  <code>{student.metadata.name}</code>
+                                </CardContent>
+                              </Card>
+                            ))}
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  )
+                )}
               </CarouselContent>
               <CarouselPrevious className="h-12 w-12 bg-sky-900 text-white" />
               <CarouselNext className="h-12 w-12 bg-sky-900 text-white" />
