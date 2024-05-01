dd50f5d539f64305f3f5c35e63b98abe7bd6a11d
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-05-01 19:24:56 +0600

    removed note and tshirt (for now)

--- packages/client/src/components/ui/user-details.tsx
+++ packages/client/src/components/ui/user-details.tsx
@@ -37,46 +37,6 @@ function UserDetails({
           <h2 className=" m-2 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
             {id}
           </h2>
-          {mode === Mode.EDITSHIRT ? (
-            <div className="m-2">edit tshirt size</div>
-          ) : (
-            <div className="m-2">Tshirt size: {tshirtSize}</div>
-          )}
-          // TODO: make tshirt editable
-        </div>
-        <div className="flex">
-          {mode === Mode.EDITNOTE ? (
-            <div className="flex flex-row">
-              <Textarea
-                className="m-4 max-h-40 max-w-5xl flex-wrap overflow-y-auto text-xl"
-                placeholder="Edit note"
-                value={displayNote}
-                onChange={(e) => setDisplayNote(e.target.value)}
-              />
-              <div className="m-4 flex flex-col justify-end">
-                <Button
-                  variant="default"
-                  onClick={() => {
-                    setMode(Mode.VIEW);
-                    // TODO: call backend to update note and display change
-                  }}
-                >
-                  Save
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <Card
-              className="m-2 max-h-40 max-w-5xl overflow-x-auto text-xl"
-              onClick={() => {
-                setMode(Mode.EDITNOTE);
-              }}
-            >
-              <p className="text-muted-foreground m-4">
-                {note == "" ? "Add note" : note}
-              </p>
-            </Card>
-          )}
         </div>
       </div>
     </>
